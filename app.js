948b20afe6ac435590167183f798dfc29b9f1229
Rafadev1213 <srafa5810@gmail.com>
2026-04-13 21:49:30 -0300

    resposividade

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let ate = parseInt(document.getElementById("ate").value);
+    let apartir = parseInt(document.getElementById("apartir").value);
+
+    return quantidade.value;
+
+    console.log(sortear());
+    
+}
+
+
+
+
+
+
+
+
